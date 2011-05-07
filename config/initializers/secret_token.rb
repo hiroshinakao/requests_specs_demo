bfd6ee5f891358cbe3d9757fa2cb3ee63bc16cd1
h-nakao <h-nakao@cloud-dev-6.local>
2011-05-07 13:41:15 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTestSample::Application.config.secret_token = '42bde23f482c3c5b170e3cec2c634038353d30571bc9dd95a9f4d936125cd4beeccdcb7e2acda043a3e4092146be8467405c70c591e11b984fd257e178d949e8'
