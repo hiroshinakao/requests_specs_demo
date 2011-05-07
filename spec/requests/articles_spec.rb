4d48aad4548389dfd8d64b1d6f1f5e5896ee2625
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2011-05-07 14:25:31 +0900

    画面遷移を伴うデータ登録のシナリオサンプル

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe "Articles" do
@@ -6,6 +7,20 @@ describe "Articles" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get articles_path
       response.status.should be(200)
+    end
+  end
+
+  describe "POST /articles" do
+    before do
+      visit "/articles/new"
+    end
+    it "新規作成画面で登録" do
+      fill_in "Title", with: "sample_title"
+      fill_in "Body", with: "sample_body"
+      click_button "Create Article"
+      page.should have_content("Article was successfully created.")
+      page.should have_content("sample_title")
+      page.should have_content("sample_body")
     end
   end
 end
